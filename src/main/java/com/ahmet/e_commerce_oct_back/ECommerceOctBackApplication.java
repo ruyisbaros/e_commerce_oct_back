afd179257b5b5d7e44807b20b566330c1899b154
ruyisbaros <ahmet.erdonmez77@gmail.com>
2022-10-14 15:49:24 +0200

    first commit

---
+++ src/main/java/com/ahmet/e_commerce_oct_back/ECommerceOctBackApplication.java
@@ -0,0 +1,13 @@
+package com.ahmet.e_commerce_oct_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceOctBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceOctBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ahmet/e_commerce_oct_back/ECommerceOctBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahmet.e_commerce_oct_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceOctBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
