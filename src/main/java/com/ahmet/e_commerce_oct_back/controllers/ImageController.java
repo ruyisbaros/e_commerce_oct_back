f2f4bb0119a1c7f8e27fd8e9cfa907eeebfb4933
ruyisbaros <ahmet.erdonmez77@gmail.com>
2022-10-23 20:57:19 +0200

    before deployment8

--- src/main/java/com/ahmet/e_commerce_oct_back/controllers/ImageController.java
+++ src/main/java/com/ahmet/e_commerce_oct_back/controllers/ImageController.java
@@ -14,7 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.util.Map;
 
-//@CrossOrigin(origins = "https://my-e-commerce-oct.herokuapp.com/",allowedHeaders = "*")
+@CrossOrigin(origins = "https://my-ecom-back.herokuapp.com/",allowedHeaders = "*")
 @RestController
 @RequestMapping("/api/v1/images")
 @AllArgsConstructor
--- src/main/java/com/ahmet/e_commerce_oct_back/controllers/UserController.java
+++ src/main/java/com/ahmet/e_commerce_oct_back/controllers/UserController.java
@@ -17,7 +17,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
-//@CrossOrigin(origins = "https://my-e-commerce-oct.herokuapp.com/",allowedHeaders = "*")
+@CrossOrigin(origins = "https://my-ecom-back.herokuapp.com",allowedHeaders = "*")
 @RestController
 @RequestMapping("/api/v1/users")
 @AllArgsConstructor
