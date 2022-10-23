573cba0ec6fd657742ca1efea33cbb7fc566365d
ruyisbaros <ahmet.erdonmez77@gmail.com>
2022-10-23 15:27:22 +0200

    before deployment3

--- src/main/java/com/ahmet/e_commerce_oct_back/controllers/ProductController.java
+++ src/main/java/com/ahmet/e_commerce_oct_back/controllers/ProductController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
-@CrossOrigin(origins = "*",allowedHeaders = "*")
+@CrossOrigin(origins = "https://my-e-commerce-oct.herokuapp.com/",allowedHeaders = "*")
 @RestController
 @RequestMapping("/api/v1/products")
 @AllArgsConstructor
