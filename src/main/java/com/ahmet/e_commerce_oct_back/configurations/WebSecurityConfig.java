63c4819f7d8cfa70d7881e14d577e5ad785abfdd
ruyisbaros <ahmet.erdonmez77@gmail.com>
2022-10-27 20:51:29 +0200

    cors policy bugs try 9

--- src/main/java/com/ahmet/e_commerce_oct_back/configurations/WebSecurityConfig.java
+++ src/main/java/com/ahmet/e_commerce_oct_back/configurations/WebSecurityConfig.java
@@ -66,8 +66,8 @@ public class WebSecurityConfig {
                 .antMatchers("/api/v1/products/admin/**").hasAuthority("Admin")
                 .antMatchers("/api/v1/users/admin/**").hasAuthority("Admin")
                 .anyRequest().authenticated()
-                .and().cors();
-        http.headers().frameOptions().sameOrigin();
+                .and().cors().disable();
+
         http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
 
@@ -75,53 +75,53 @@ public class WebSecurityConfig {
 
     }
 
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
-            throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
-    }
-
-    @Bean
-    public DaoAuthenticationProvider daoAuthenticationProvider() {
-        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        provider.setPasswordEncoder(passwordEncoder);
-        provider.setUserDetailsService(userService);
-        return provider;
-    }
-
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration corsConfiguration = new CorsConfiguration();
-        // Below config will allow only following origines from web browser
-        corsConfiguration.setAllowedOrigins(Collections.singletonList("https://my-ecom-back.herokuapp.com"));
-        // Whether user credentials are supported. By default, do not support
-        // If you want to allow credentials then set it true
-        corsConfiguration.setAllowCredentials(true);
-
-        // below will not allow DELETE methods, if you want then add DELETE also
-        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "DELETE", "POST", "PUT", "PATCH", "OPTION"));
-
-        // Below will set allowed headers list, Other headers will not be supportedOrigin, " +
-        ////                    "X-Requested-With, Content-Type, Accept
-        corsConfiguration.setAllowedHeaders(Arrays.asList("Accept", "X-Requested-With", "Authorization", "Content-Type", "apikey", "tenantId"));
-
-        UrlBasedCorsConfigurationSource corsConfigurationSource = new UrlBasedCorsConfigurationSource();
-
-        // This will register above configurations on all resources from the root
-        // If you want different rules for different resources then create separate configuration
-        // and register on separate resource path uri
-        corsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
-        return corsConfigurationSource;
-    }
-
-    @Bean
-    public FilterRegistrationBean corsFilterRegistrationBean(CorsConfigurationSource corsConfigurationSource) {
-        CorsFilter corsFilter = new CorsFilter(corsConfigurationSource);
-        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
-        filterRegistrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
-        filterRegistrationBean.setFilter(corsFilter);
-        return filterRegistrationBean;
-    }
+//    @Bean
+//    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+//            throws Exception {
+//        return authenticationConfiguration.getAuthenticationManager();
+//    }
+//
+//    @Bean
+//    public DaoAuthenticationProvider daoAuthenticationProvider() {
+//        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+//        provider.setPasswordEncoder(passwordEncoder);
+//        provider.setUserDetailsService(userService);
+//        return provider;
+//    }
+//
+//    @Bean
+//    public CorsConfigurationSource corsConfigurationSource() {
+//        CorsConfiguration corsConfiguration = new CorsConfiguration();
+//        // Below config will allow only following origines from web browser
+//        corsConfiguration.setAllowedOrigins(Collections.singletonList("https://my-ecom-back.herokuapp.com"));
+//        // Whether user credentials are supported. By default, do not support
+//        // If you want to allow credentials then set it true
+//        corsConfiguration.setAllowCredentials(true);
+//
+//        // below will not allow DELETE methods, if you want then add DELETE also
+//        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "DELETE", "POST", "PUT", "PATCH", "OPTION"));
+//
+//        // Below will set allowed headers list, Other headers will not be supportedOrigin, " +
+//        ////                    "X-Requested-With, Content-Type, Accept
+//        corsConfiguration.setAllowedHeaders(Arrays.asList("Accept", "X-Requested-With", "Authorization", "Content-Type", "apikey", "tenantId"));
+//
+//        UrlBasedCorsConfigurationSource corsConfigurationSource = new UrlBasedCorsConfigurationSource();
+//
+//        // This will register above configurations on all resources from the root
+//        // If you want different rules for different resources then create separate configuration
+//        // and register on separate resource path uri
+//        corsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+//        return corsConfigurationSource;
+//    }
+//
+//    @Bean
+//    public FilterRegistrationBean corsFilterRegistrationBean(CorsConfigurationSource corsConfigurationSource) {
+//        CorsFilter corsFilter = new CorsFilter(corsConfigurationSource);
+//        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+//        filterRegistrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+//        filterRegistrationBean.setFilter(corsFilter);
+//        return filterRegistrationBean;
+//    }
 
 //    @Override
 //    protected void configure(HttpSecurity http) throws Exception {
