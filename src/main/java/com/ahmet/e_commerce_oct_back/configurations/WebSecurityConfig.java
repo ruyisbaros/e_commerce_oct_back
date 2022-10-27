bacbc79cd632c3f1699c68cee6848913163881e5
ruyisbaros <ahmet.erdonmez77@gmail.com>
2022-10-27 21:57:44 +0200

    cors policy bugs try 15

--- src/main/java/com/ahmet/e_commerce_oct_back/configurations/WebSecurityConfig.java
+++ src/main/java/com/ahmet/e_commerce_oct_back/configurations/WebSecurityConfig.java
@@ -121,7 +121,7 @@ public class WebSecurityConfig {
 
         @Override
         public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
-            HttpServletResponse response = (HttpServletResponse) req;
+            HttpServletResponse response = (HttpServletResponse) res;
             response.setHeader("Access-Control-Allow-Origin", "*");
             response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
             response.setHeader("Access-Control-Max-Age", "3600");
